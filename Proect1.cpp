9cd50fe2f16a6b62b08c74dc40789bb7460dd165
illbeurs <91798939+illbeurs@users.noreply.github.com>
2021-10-02 23:58:58 +0400

    Add files via upload

---
+++ Proect1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int a;
+    int b;
+    cin >> a;
+    b = abs(a);
+    if ((b / 100 >= 1) && (b / 100 <= 9))
+        cout << "Число " << a << " - трёхначное";
+    else
+        cout << "Число " << a << " - НЕ трёхначное";
+
+}
