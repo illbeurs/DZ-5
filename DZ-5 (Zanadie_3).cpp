b4f07f7c3177c2a0615263abeb5dbab1a2c3b1cd
illbeurs <91798939+illbeurs@users.noreply.github.com>
2021-12-13 23:43:23 +0300

    Update DZ-5 (Zanadie_3).cpp

--- DZ-5 (Zanadie_3).cpp
+++ DZ-5 (Zanadie_3).cpp
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
+void initials(char str[], const int n, char* null) {
+	char* x = strtok_s(str, " ", &null);
+	char* c = x;
+	x = strtok_s(NULL, " ", &null);
+	char a[] = { x[0],'.', '\0' };
+	x = strtok_s(NULL, " ", &null);
+	char b[] = { x[0],'.','\0' };
+	cout << a << b << " " << c;
+}
 int main() {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
@@ -12,11 +21,5 @@ int main() {
 	char* o = NULL;
 	cout << "Введите ФИО: " << endl;
 	cin.getline(str, n);
-	char* x = strtok_s(str, " ", &o);
-	char* c = x;
-	x = strtok_s(NULL, " ", &o);
-	char a[] = { x[0],'.', '\0' };
-	x = strtok_s(NULL, " ", &o);
-	char b[] = { x[0],'.','\0' };
-	cout << a << b << " " << c;
+	initials(str, n, o);
 }
